4d92a6a92eeb551a4ec404f346add0d0afa138aa
Ashish Satapathy <ashish.satapathy2014@gmail.com>
2022-09-05 18:14:07 +0530

    Exchanger

---
+++ src/main/java/com/arsatapathy/exchanger/ExchangerTestApp.java
@@ -0,0 +1,58 @@
+package com.arsatapathy.exchanger;
+
+import java.util.concurrent.Exchanger;
+
+public class ExchangerTestApp {
+    public static void main(String[] args) {
+        Exchanger<String> exchanger = new Exchanger<>();
+
+        new Thread(new StringGenerator(exchanger)).start();
+        new Thread(new EmptyStringGenerator(exchanger)).start();
+    }
+
+    private static class StringGenerator implements Runnable {
+        private Exchanger<String> exchanger;
+        private String str;
+
+        public StringGenerator(Exchanger<String> exchanger) {
+            this.exchanger = exchanger;
+            this.str = new String();
+        }
+
+        @Override
+        public void run() {
+            char ch = 'A';
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 5; j++) {
+                    str += ch++;
+                }
+                try {
+                    str = exchanger.exchange(str);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static class EmptyStringGenerator implements Runnable {
+        private final Exchanger<String> exchanger;
+        private String str;
+
+        public EmptyStringGenerator(Exchanger<String> exchanger) {
+            this.exchanger = exchanger;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 3; i++) {
+                try {
+                    str = exchanger.exchange("");
+                    System.out.println("Got : " + str);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
