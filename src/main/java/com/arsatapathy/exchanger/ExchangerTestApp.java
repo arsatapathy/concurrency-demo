68b13978218f17d94e1203285b95b7c7c1823f94
Ashish Satapathy <ashish.satapathy2014@gmail.com>
2022-09-05 18:27:31 +0530

    Exchanger

--- src/main/java/com/arsatapathy/exchanger/ExchangerTestApp.java
+++ src/main/java/com/arsatapathy/exchanger/ExchangerTestApp.java
@@ -27,7 +27,9 @@ public class ExchangerTestApp {
                     str += ch++;
                 }
                 try {
+                    System.out.println("Put : " + str);
                     str = exchanger.exchange(str);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
